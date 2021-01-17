de177db5595458165070b74337a304c805a52a34
Liam Emmart <l.emmart@outlook.com>
2021-01-17 14:52:16 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setuptools.setup(
     name="metaedit",
     version="0.0.1",
     author="Liam Emmart",
-    author_email="liam.emmart@gmail.com",
+    author_email="l.emmart@outlook.com",
     description="Image and metadata attribute viewer and editor.",
     long_description_content_type="text/markdown",
     url="https://github.com/Lemmart/metaedit",
